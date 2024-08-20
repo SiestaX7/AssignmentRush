faca762db6bf3932b4fe8bd5c7d1cc7574ab3d6c
Ivan Cheah <5a13ivanckj@gmail.com>
2024-08-20 16:19:38 +0800

    Feat: Major Changes (Draw, Import sys, Buttons, Variables etc.)
    1. Import sys
    2. Added Variables
    3. Buttons
    4. Rearranged the codes and added text
    5. Basically made a functional game for now ig.

--- Main Game.py
+++ Main Game.py
@@ -1,5 +1,6 @@
 # Initiating Game
 import pygame
+import sys
 pygame.init()
 
 # Colour Library
@@ -12,31 +13,55 @@ purple = (127, 0, 255)
 orange = (255, 165, 0)
 
 #Screen
-screen = pygame.display.set_mode([800, 800])
+width, height = 800, 800
+screen = pygame.display.set_mode([width, height])
 pygame.display.set_caption("Assignment Rush")
 background = black
 framerate = 60
 font = pygame.font.Font('Lato-Bold.ttf', 30)
 timer = pygame.time.Clock()
 
-# Defining "Draw"
-def draw(colour, y_coord, value):
-    pygame.draw.circle(screen, colour, (100, y_coord), 50, 5)
-    value_text = (font.render(str(value), True, white))
-    screen.blit(value_text, (90, y_coord - 10))
+# Game Variables
+money = 0
+click_value = 1
+
+# Button Size
+button_width, button_height = 200, 50
+button_x = (width - button_width) //2
+button_y = (height - button_width) //2
+
 
 # Game Running Loop
 running = True
 while running:
-    timer.tick(framerate)
+    screen.fill(black)
+
+    # Handling Conditions
     for event in pygame.event.get():
+
+        # Quit
         if event.type == pygame.QUIT:
             running = False
+        
+        # When Click
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if button_x <= mouse_x <= button_x + button_width and button_y <= mouse_y <= button_y + button_width:
+                money += click_value
+    
+    # Draw Button
+    pygame.draw.rect(screen, green, (button_x, button_y, button_width, button_height))
 
-    screen.fill(background)
-    draw(green, 100, 1)
+    # Show Money
+    money_text = font.render(f"Marks: {money}", True, white)
+    screen.blit(money_text, (20, 20))
 
+    # Update Display
     pygame.display.flip()
 
-pygame.quit()
+    # Cap Framerate
+    pygame.time.Clock().tick(framerate)
 
+
+pygame.quit()
+sys.exit
